1b3d67ab5f9b079991c44755ae27517d4adec847
Adrian Llanes <allanes@herrera.unt.edu.ar>
2021-10-01 07:18:24 -0300

    fixes

--- antenna_design.py
+++ antenna_design.py
@@ -38,9 +38,9 @@ def option_one(cfg):
     """
     ETAPA 1. Genera datos para Heatmap
     """
-    dataset = cfg.configurar_log(etapa=1)
+    dataset = cfg.configure_log(option=1)
     
-    [param1_name, param2_name] = get_params_names(cfg.disposicion)
+    [param1_name, param2_name] = get_params_names(cfg.distribution)
     progreso_maximo = cfg.get_max_progress()
     aux_progreso = 0
     for aux_param1 in range(cfg.get_param1_initial_value(), cfg.get_param1_final_value()):
@@ -48,11 +48,11 @@ def option_one(cfg):
         for aux_param2 in range(cfg.get_param2_initial_value(), cfg.get_param2_final_value()):
             logging.info(f'----------Cantidad de Elementos en Parametro 2: {aux_param2} -------------')
             [elev_w, azim_w] = array_evaluation_process(
-                distribution_type=cfg.disposicion,
-                separation=cfg.separacion,
+                distribution_type=cfg.distribution,
+                separation=cfg.separation,
                 param1=aux_param1,
                 param2=aux_param2,
-                aiming=cfg.apuntamiento,
+                aiming=cfg.aiming,
                 plot=False
             )
             cfg.log_widths(theta=elev_w, phi=azim_w)
@@ -63,15 +63,15 @@ def option_one(cfg):
     return dataset
 
 
-def option_two(configuracion):
+def option_two(config):
     """
     ETAPA 2. Evalua la respuesta en frecuencia    
     """    
-    rango_frecuencias = [configuracion.frecuencia_disenio,2e6,3e6,4e6,5e6,6e6,7e6,8e6,9e6,10e6,11e6,12e6,13e6,14e6,15e6] # ,16e6,17e6,18e6,19e6,20e6
+    rango_frecuencias = [config.design_frequency,2e6,3e6,4e6,5e6,6e6,7e6,8e6,9e6,10e6,11e6,12e6,13e6,14e6,15e6] # ,16e6,17e6,18e6,19e6,20e6
     freq = np.array(rango_frecuencias)
-    Dn,d = antenna_core_functions.denormalise_frequency(freq,configuracion.separacion)
+    Dn,d = antenna_core_functions.denormalise_frequency(freq,config.separation)
     
-    dataset = configuracion.configurar_log(etapa=2, separacion_metros=d[0])
+    dataset = config.configure_log(option=2, separation_m=d[0])
     
     # Recalculo los anchos para las frecuencias desnormalizadas
     anchos_elevacion = []
@@ -81,17 +81,17 @@ def option_two(configuracion):
         logging.info(f"Distancia en Lambda: {Dn[index]}")
         logging.info(f"Frecuencia: {freq[index]}")
         [elev_w, azim_w] = array_evaluation_process(
-            distribution_type=configuracion.disposicion, 
+            distribution_type=config.distribution, 
             separation=Dn[index], 
-            param1=configuracion.parametro1, 
-            param2=configuracion.parametro2,
-            aiming=configuracion.apuntamiento,
+            param1=config.parameter1, 
+            param2=config.parameter2,
+            aiming=config.aiming,
             plot=False
         )
 
         anchos_elevacion.append(elev_w)
         anchos_azimut.append(azim_w)
-        configuracion.log_widths(theta=elev_w, phi=azim_w)
+        config.log_widths(theta=elev_w, phi=azim_w)
     
         aux_progress += 1
         print(f'Progreso: {100*aux_progress/len(Dn):.1f}%')
@@ -101,14 +101,14 @@ def option_two(configuracion):
 
 
 def option_three(config):
-    filename = config.configurar_log(etapa=3)
+    filename = config.configure_log(option=3)
     
     array_evaluation_process(
-        distribution_type=config.disposicion, 
-        separation=config.separacion, 
-        param1=config.parametro1, 
-        param2=config.parametro2,
-        aiming=config.apuntamiento,
+        distribution_type=config.distribution, 
+        separation=config.separation,
+        param1=config.parameter1, 
+        param2=config.parameter2,
+        aiming=config.aiming,
         plot=True
         )
 
